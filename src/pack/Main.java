5420f43f6e2c95dfc79d15d7269b97aa151c2da0
corina.tanase <corinatanase98@gmail.com>
2020-11-08 19:43:07 +0200

    Refacere main Detalii commit Signed-off-by: corina.tanase <corinatanase98@gmail.com>

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("proiect cu masini");
+        System.out.println("Proiect cu masini\n");
+        System.out.println("Cea mai smechera reprezentanta");
     }
 }
