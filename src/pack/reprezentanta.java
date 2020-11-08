e584b90d02125cf5adffa1110e960e88b4de0d08
titus <bogdan.pirjol123@gmail.com>
2020-11-08 20:35:17 +0200

    adaugare settere
    am adaugat settere pt nr de masini si nume reprezentanta
    
    Signed-off-by: basicjunior <stanmihailalexandru@gmail.com>

--- src/pack/reprezentanta.java
+++ src/pack/reprezentanta.java
@@ -5,4 +5,12 @@ import java.util.ArrayList;
 public class reprezentanta {
     private String NumeReprezentanta;
     private int NumarMasini;
+
+    public String getNumeReprezentanta() {
+        return NumeReprezentanta;
+    }
+
+    public int getNumarMasini() {
+        return NumarMasini;
+    }
 }
